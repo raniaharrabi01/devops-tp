3e9617acc84dc35e6a97a322dd7d8f3d2a452ae7
rania <harrabirania2@gmail.com>
2025-02-25 17:59:07 +0100

    add status

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,2 +1,15 @@
 # app.py
-print("Hello, CI/CD!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+@app.route('/status')
+def status():
+    return {"status": "Application is running!"}
+
+
+if __name__ == "__main__":
+    app.run()
